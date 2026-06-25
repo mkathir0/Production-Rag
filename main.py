046b6fcdf9cc77b4c05dc6c0915e7f990f261a65
mkathir0 <mithrajit07@gmail.com>
2026-06-25 14:53:46 +0530

    Groq instead of openai

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+llm = ChatGroq(model_name="llama-3.1-8b-instant", temperature=0)
+response = llm.invoke("Hello, how are you?")
+print(response.content)
+
+
 def main():
     print("Hello from rag!")
 
